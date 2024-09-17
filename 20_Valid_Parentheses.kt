feba33de015ac795736e2cae0e29101b13aea8f6
Developer1449 <m.a1449pithadia@gmail.com>
2024-09-16 20:17:03 -0400

    Problem #20 Solution

---
+++ 20_Valid_Parentheses.kt
@@ -0,0 +1,21 @@
+import java.util.Stack
+
+class Solution {
+    fun isValid(s: String): Boolean {
+        val stack = Stack<Char>()
+        val map = hashMapOf('(' to ')', '[' to ']', '{' to '}')
+
+        for (c in s) {
+            when {
+                map.containsKey(c) -> stack.push(c)  // Push the opening bracket onto the stack
+                map.containsValue(c) -> {
+                    if (stack.isEmpty() || map[stack.pop()] != c) {
+                        return false  // Mismatch or unbalanced
+                    }
+                }
+            }
+        }
+        
+        return stack.isEmpty()  // True if all brackets are matched
+    }
+}
