51f7a52bab65ff6d37f20c65acab42ef4418cabe
Developer1449 <m.a1449pithadia@gmail.com>
2024-09-16 20:11:45 -0400

    Two Sum Solution

---
+++ 1_Two_Sum.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = HashMap<Int,Int>()
+    
+        for((index, value) in nums.withIndex()){
+            val k = target - value
+            if(map.containsKey(k)){
+                return intArrayOf(map[k]!!, index)
+            }
+            map[value] = index
+        } 
+        return intArrayOf()
+    }
+}
