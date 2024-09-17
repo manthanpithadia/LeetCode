473c1f71c4b422afcb98da1cb6f7ad81ae80051f
Developer1449 <m.a1449pithadia@gmail.com>
2024-09-16 20:15:15 -0400

    Problem #7 Solution

---
+++ 7_Reverse_Integer.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun reverse(x: Int): Int {
+        var num = x
+        var reversed = 0
+
+        while(num!=0){
+            val digit = num % 10
+            num /= 10
+
+            // Check the overflow
+            if(reversed > Int.MAX_VALUE/10 || ((reversed == Int.MAX_VALUE/10) && digit > 7)) return 0
+            if(reversed < Int.MIN_VALUE/10 || ((reversed == Int.MIN_VALUE/10) && digit < -8)) return 0
+
+            reversed = reversed * 10 + digit
+        }
+        return reversed
+    }
+}
