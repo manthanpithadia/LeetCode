a8cc051f4cb45ffded20476960778a5ae7640c33
Developer1449 <m.a1449pithadia@gmail.com>
2024-09-16 20:02:38 -0400

    first commit

---
+++ 561_ArrayPartition.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun arrayPairSum(nums: IntArray): Int {
+        nums.sort()
+        var sum = 0
+        for(i in nums.indices step 2){
+            sum += minOf(nums[i],nums[i+1])
+        }
+        return sum
+    }
+}
