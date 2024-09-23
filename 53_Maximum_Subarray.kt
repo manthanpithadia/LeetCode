897a3d28186224880121010c8759355fbc313da3
Developer1449 <m.a1449pithadia@gmail.com>
2024-09-23 16:15:51 -0400

    kadane's Algorithm updated -> Solution #53

--- 53_Maximum_Subarray.kt
+++ 53_Maximum_Subarray.kt
@@ -10,4 +10,18 @@ fun maxSubArray(nums: IntArray): Int {
         maxSum = max(currentSum, maxSum)
         }
         return maxSum
-    }
+}
+
+// Kadane's Algorithm
+fun kadaneAlgorithm(nums: IntArray): Int{
+        var maxSum = Int.MIN_VALUE
+        var currentSum = 0
+
+        for(i in nums){
+                currentSum += i
+                maxSum = max(currentSum, maxSum)
+
+                if(currentSum<0) currentSum = 0
+        }
+        return maxSum
+}
