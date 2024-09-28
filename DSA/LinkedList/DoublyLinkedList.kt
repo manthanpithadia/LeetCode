372229ed2c514f0945e1cbc2745e0eac788f8fa9
Developer1449 <m.a1449pithadia@gmail.com>
2024-09-28 16:41:32 -0400

    LinkedList

---
+++ DSA/LinkedList/DoublyLinkedList.kt
@@ -0,0 +1,84 @@
+package DSA.LinkedList
+
+fun main() {
+    val list = DoublyLinkedList()
+    list.addNodeDoubly(2)
+    list.addNodeDoubly(3)
+    list.addNodeDoubly(6)
+    list.addNodeDoubly(2)
+    list.addNodeDoubly(3)
+    list.display()
+}
+
+data class DNode(
+    var data: Int,
+    var next: DNode? = null,
+    var prev: DNode? = null
+)
+
+interface IDoublyLinkedList{
+    var head: DNode?
+    var tail: DNode?
+    fun addNodeDoubly(value:Int)
+    fun deleteNodeDoubly(value: Int)
+    fun display()
+}
+
+class DoublyLinkedList: IDoublyLinkedList{
+    override var head: DNode? = null
+    override var tail: DNode? = null
+
+    override fun addNodeDoubly(value: Int) {
+        // Create a new node.
+        var newNode = DNode(value)
+
+        //If the list is empty, set both head and tail to the new node.
+        if(head==null){
+            head = newNode
+            tail = newNode
+        }
+        else{
+            tail?.next = newNode
+            newNode.prev = tail
+            tail = newNode
+        }
+    }
+
+    override fun deleteNodeDoubly(value: Int) {
+        if(head==null) return
+
+        if(head?.data == value) {
+            head = head?.next
+            head?.prev = null
+        }
+        var current = head
+
+        while(current != tail && current?.next?.data != value){
+            current = current?.next
+        }
+
+        if(current!=tail){
+            current?.next = current?.next?.next
+            if(current?.next!=null) {
+                current?.next?.prev = current
+                tail = current.next
+            }
+            else tail = current
+        }
+    }
+
+    override fun display() {
+        var current = head
+        while (current!=null){
+            print("${current.data}")
+            current = current.next
+            if(current!=null){
+                print(", ")
+            }
+            else{
+                print(".")
+            }
+        }
+    }
+
+}
---
+++ DSA/LinkedList/LinkedList.kt
@@ -0,0 +1,118 @@
+package DSA.LinkedList
+
+fun main() {
+    val list = LinkedList()
+    list.addNode(2)
+    list.addNode(3)
+    list.addNode(6)
+    list.addNode(2)
+    list.addNode(3)
+    list.reverseList()
+    list.displayNodes()
+}
+
+data class Node(
+    var data: Int,
+    var next: Node? = null
+)
+
+internal class LinkedList(): ILinkedList{
+    override var head: Node? = null
+    override var tail: Node? = null
+
+    override fun addNode(data: Int) {
+        val newNode = Node(data)
+        if(head == null){
+            head = newNode
+            tail = newNode
+        }
+        else{
+            tail?.next = newNode
+            tail = newNode
+        }
+    }
+
+    override fun displayNodes() {
+        var ptr = head
+        while(ptr!=null){
+            print("${ptr?.data}")
+            ptr = ptr.next
+            if(ptr!=null){
+                print(", ")
+            }
+            else{
+                print(".")
+            }
+        }
+    }
+
+    override fun deleteNode(value: Int) {
+        //If the list is empty, return.
+        if (head==null) return
+
+        //If the node to be deleted is the head, update the head to head.next.
+        if(head?.data == value){
+            head = head?.next
+            if(head==null) tail = null
+            return
+        }
+
+        //Traverse the list to find the node before the node to be deleted.
+        var current = head
+        while (current!=tail  && current?.next?.data!=value){
+            current = current?.next
+        }
+
+        if(current!=tail){ // I am at the 1 node before the deleting node
+            current?.next = current?.next?.next
+            //If deleting the tail, update the tail pointer.
+            if(current?.next == null) tail = current
+        }
+    }
+
+    override fun searchNode(value: Int): Boolean {
+        //Start from the head.
+        var current = head
+        var index = 0
+        //Traverse through the list while comparing each node's data with the target value.
+        while (current!=null){
+            if(current.data == value){
+                //Return true if found,
+                println("value Found at $index")
+                return true
+            }
+            current = current.next
+            index++
+        }
+
+        //else return false.
+        return false
+    }
+
+    override fun reverseList() {
+        //Use three pointers: prev (null), current (head), and next (null).
+        var prev:Node? = null
+        var current:Node? = head
+        var next:Node? = null
+
+        //Traverse the list, reversing the next pointers.
+        tail = head
+        while (current!=null){
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        }
+        head = prev
+    }
+}
+
+interface ILinkedList{
+    var head:Node?
+    var tail: Node?
+    fun addNode(data:Int)
+    fun displayNodes()
+    fun deleteNode(value: Int)
+    fun searchNode(value: Int): Boolean
+    fun reverseList()
+}
